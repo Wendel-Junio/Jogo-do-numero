b50f923088abf2a1ba1b91d9ee87bf4762ec013e
Wendel-Junio <61203054+Wendel-Junio@users.noreply.github.com>
2023-05-22 23:59:52 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+print("________________________________________")
+print("Bem Vindo Wendel, advinhe o numero fi")
+print("________________________________________")
+Chute = input("Digite o numero:")
+
+Numero = 24
+
+if Chute == Numero:
+    print("PARABENS KRL VC ACERTOU")
+else:
+    print("Voce errou tenta de novo")
+   
+
